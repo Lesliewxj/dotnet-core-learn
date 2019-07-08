41066197232347c3098ac733cc24f41942fb4b00
Leslie <2445054599@qq.com>
2019-07-08 20:11:25 +0800

    dotnet learn version1.0

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(6+4);
+            Console.WriteLine("dotnetcore learn");
+        }
+    }
+}
